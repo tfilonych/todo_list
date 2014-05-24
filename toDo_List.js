dffca676e06ddd198d88c203d5ad1006ba131514
Tanya Filonych <tfilonych@mail.ru>
2014-05-24 17:32:14 +0300

    add id to image and div

--- toDo_List.js
+++ toDo_List.js
@@ -1,6 +1,10 @@
-function addText() {
+var counter = 0;
+	
+function addText(){
+	counter++;
 	var div = document.createElement('div');
 	div.setAttribute('class', 'newText');
+	div.setAttribute('id', 'row' + counter);
 	var answerField = document.getElementById("answer");
 	var someText = answerField.value;
 	var txt = document.createTextNode(someText);
@@ -10,9 +14,15 @@ function addText() {
 	var image = document.createElement('img');
 	image.setAttribute('src', 'close.png');
 	image.setAttribute('class', 'kartynka');
-	var imageOnPage = document.getElementById("wrap");
+	image.setAttribute('id', 'button' + counter);
 	div.appendChild(image);
 	answerField.value = '';
-	
+}
 
+function deleteRow(button_id){
+	var wrapper = document.getElementById("wrapper");
+	var button = document.getElementById(button_id);
+	var div = button;
+	button.div.removeChild(button);
+	
 }
